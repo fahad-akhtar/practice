c60e17026ea77fad606c0c0042a4016639f7fa9f
fahad-akhtar <fahad201308@gmail.com>
2024-06-19 14:58:31 +0530

    JAVA for loop program

---
+++ InterviewQuestions/src/basic/for_loop.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class for_loop {
+	
+	public static void main(String args[])
+	{
+		int a[]= {1,6,5,9,4,7,3};
+		System.out.println("For loop = ");
+		for(int i = 0 ; i< a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
