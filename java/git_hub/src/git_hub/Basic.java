d8ff0f599ca460b78bed6a436195e90e100854ba
md-shazeb <mdshazeb151@gmail.com>
2024-07-27 00:55:10 +0530

    Added some lines

---
+++ java/git_hub/src/git_hub/Basic.java
@@ -0,0 +1,13 @@
+package git_hub;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 99;
+int b = 34;
+  System.out.println("the sum of two numbers is "+(a+b));
+  
+	}
+
+}
